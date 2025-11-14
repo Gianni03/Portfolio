a539ffddf5a123b6274c5524336532552f7121c9
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-11-14 05:45:02 -0300

    starter files for new portfolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to my Portfolio</h1>
+      <p>This is a simple portfolio website built with Vite and React.</p>
+    </>
+  )
+}
+
+export default App
