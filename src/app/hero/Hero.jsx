a2429f5c6eaa6a7d4a2e6e356403326367360ca3
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-12-29 19:53:50 -0300

    create hero and aboutMe components

---
+++ src/app/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import Section from '../../ui/Section';
+
+export default function Hero() {
+  return (
+
+   <section id="hero">
+     <h1 className="text-5xl font-bold mb-4">Welcome to My Portfolio</h1>
+     <p className="text-lg text-neutral-300">
+       I'm a frontend developer with a passion for building beautiful and
+       functional web applications.
+     </p>
+   </section>
+  )
+}
--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -1,12 +1,14 @@
 import Section from '../../ui/Section';
+import Hero from '../hero/Hero';
+import AboutMe from '../about/AboutMe';
 import Projects from '../projects/Projects';
 import Studies from '../studies/Studies';
 
 export default function Home() {
   return (
     <>
-      <Section title="Hero" id="hero"></Section>
-      <Section title="About Me" id="about"></Section>
+      <Hero />
+      <AboutMe />
       <Section title="Skills" id="skills"></Section>
 
       <Section title="Projects" id="projects">
